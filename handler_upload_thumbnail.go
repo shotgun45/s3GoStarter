d4e97a3bc3818bb973d247fa04afaa02c8cc14f4
shotgun45 <jipeles@yahoo.com>
2025-09-01 13:08:18 -0400

    chapter 1 lesson 5

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,69 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// Parse multipart form data
+	const maxMemory = 10 << 20 // 10MB
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Get the image data from the form
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error retrieving thumbnail file", err)
+		return
+	}
+	defer file.Close()
+
+	// Get the media type from the Content-Type header
+	mediaType := fileHeader.Header.Get("Content-Type")
+	fmt.Println("Thumbnail media type:", mediaType)
+
+	// Read all image data into a byte slice
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading image data", err)
+		return
+	}
+	fmt.Printf("Read %d bytes of image data\n", len(imageData))
+
+	// Get the video's metadata from the database
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error fetching video metadata", err)
+		return
+	}
+	if video.ID == uuid.Nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", nil)
+		return
+	}
+
+	// Check if the authenticated user is the video owner
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not the owner of this video", nil)
+		return
+	}
+
+	// Save the thumbnail to the global map
+	thumb := thumbnail{
+		data:      imageData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoID] = thumb
+
+	// Update the video metadata with the new thumbnail URL
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
+	video.ThumbnailURL = &thumbnailURL
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video metadata", err)
+		return
+	}
+
+	// Respond with updated video metadata as JSON
+	respondWithJSON(w, http.StatusOK, video)
 }
